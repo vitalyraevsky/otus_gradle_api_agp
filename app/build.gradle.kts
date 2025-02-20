98dd16400d9b7b4cc7e5c38401034cbe7b6f9829
Vitaly Raevsky <vitaly.raevsky@gmail.com>
2025-02-20 22:29:45 +0400

    use AGP in gradle scripts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,3 +1,5 @@
+import org.jetbrains.kotlin.gradle.dsl.kotlinExtension
+
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
@@ -15,11 +17,32 @@ android {
         targetSdk = 35
         versionCode = 1
         versionName = "1.0"
-
+        resourceConfigurations += listOf("en", "ru")
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        buildConfigField("String", "API_KEY", "\"somekey\"")
     }
 
+/*
+    signingConfigs {
+        named("debug") {
+            storeFile = file("application1.jks")
+            storePassword = "password1"
+            keyAlias = "application1"
+            keyPassword = "password1"
+        }
+        register("release") {
+            storeFile = file("applicationRelease.jks")
+            storePassword = "password1"
+            keyAlias = "application1"
+            keyPassword = "password1"
+        }
+    }
+*/
+
+    buildFeatures.buildConfig = true
+
     buildTypes {
+
         named("release") {
             isMinifyEnabled = false
             proguardFiles(
@@ -28,9 +51,60 @@ android {
             )
         }
         named("debug") {
-
+            //signingConfig = signingConfigs.findByName("debug")
+            applicationIdSuffix = ".debug"
+            isMinifyEnabled = false
+            isDebuggable = true
+            isShrinkResources = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+        //create("feature") {
+        register("feature") {
+            initWith(getByName("debug"))
+            matchingFallbacks.add("debug")
+            applicationIdSuffix = ".preview"
         }
     }
+
+    flavorDimensions += listOf("mode", "type")
+
+    productFlavors {
+        create("free") {
+            dimension = "mode"
+            applicationIdSuffix = ".free"
+        }
+        register("paid") {
+            dimension = "mode"
+        }
+        register("huawei") {
+            applicationIdSuffix = ".huawei"
+            dimension = "type"
+        }
+        register("googlw") {
+            dimension = "type"
+        }
+    }
+
+    sourceSets {
+        named("main") {
+            res.srcDirs(
+                "src/main/res",
+                "src/ai/res"
+            )
+            java.srcDirs(
+                "src/main/java",
+                "src/ai/java"
+            )
+            assets.srcDirs(
+                "src/main/assets",
+                "src/ai/assets"
+            )
+        }
+    }
+
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_17
         targetCompatibility = JavaVersion.VERSION_17
@@ -39,11 +113,60 @@ android {
         jvmTarget = "17"
     }
 
+    kotlinExtension.apply {
+        jvmToolchain(17)
+    }
+
     publishing {
+/*
         singleVariant("debug") {
             publishApk()
         }
+*/
     }
+
+    splits {
+        abi {
+            isEnable = true
+            isUniversalApk = true
+            reset()
+            include("x86", "x86_64", "armeabi-v7a","arm64-v8a")
+        }
+        density {
+            isEnable = true
+            compatibleScreens("small", "normal", "large", "xlarge")
+        }
+    }
+}
+
+androidComponents {
+
+    finalizeDsl { // Вызывается после генерации конфигурации проета
+        it.buildFeatures.buildConfig = true
+    }
+
+    beforeVariants() { }  // Вызывается до генерации конфигурации проета
+    beforeVariants { variant ->
+        variant.minSdk = 26
+    }
+    beforeVariants { variant ->
+        if (variant.productFlavors.containsAll(
+            listOf("mode" to "free", "type" to "huawei")
+        )) {
+            variant.enable = false
+        }
+    }
+    val debug = selector().withBuildType("debug")
+    beforeVariants(debug) { variantBuilder ->
+        println("Called with variant : ${variantBuilder.name}")
+    }
+
+    onVariants { }
+    onVariants(debug) { }
+    onVariants(selector().withBuildType("release")) {  variant -> // Вызывается после генерации конфигурации проета
+        println("Called with ready variant : ${variant.name}")
+    }
+
 }
 
 dependencies {
@@ -62,7 +185,7 @@ afterEvaluate {
     publishing {
         publications {
             register<MavenPublication>("debugMaven") {
-                from(components["debug"])
+//                from(components["debug"])
                 groupId = "ru.otus.gradleapi"
                 artifactId = "app"
             }
